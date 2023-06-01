c2cd95ccfd6c97c32db04c2dd50fe3ea1f091e07
jake <jake@ubuntudev.myguest.virtualbox.org>
2023-05-31 21:08:25 -0400

    Fix fuzzer

--- mayhem/signxml_fuzz.py
+++ mayhem/signxml_fuzz.py
@@ -57,7 +57,7 @@ uCnX6siFNDlUAg==
 
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
-    ran = 0#fdp.ConsumeIntInRange(0, )
+    ran = 1#fdp.ConsumeIntInRange(0, )
     xml_str = fdp.ConsumeBytes(fdp.remaining_bytes())
     xml_str = b'<?xml version="1.0"?><data>' + xml_str + b'</data>'
     try:
